95608e81f1931cb9a91891428258f99078d88667
Wahyu <wahyutptp20@gmail.com>
2023-04-04 14:03:58 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        backgroundColor: Colors.blueAccent.shade700,
+        title: Text('Calculator'),
+      )),
+    );
+  }
+}
